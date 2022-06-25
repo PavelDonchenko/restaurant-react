ebba526f3846d0598a0605a14d27377ceb6a9ef8
PavelDonchenko <przmld033@gmail.com>
2022-06-26 00:58:40 +0300

    added all components and styles  on home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 import './scss/app.scss';
 import './js/libs/dynamic_adapt';
+
+import Categories from "./components/Categories";
+import ColdDishes from "./components/ColdDishes";
+import HotDishes from "./components/HotDishes";
+import About from "./components/About";
+import Map from "./components/Map";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -9,8 +18,15 @@ function App() {
       <div className="wrapper">
         <Header />
         <main className="page">
+          <Banner />
+          <Categories />
+          <ColdDishes />
+          <HotDishes />
+          <About />
+          <Map />
 
         </main>
+        <Footer />
       </div>
 
     </div>
