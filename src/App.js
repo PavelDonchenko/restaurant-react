0aa06f6e58c1b8679412ee6e69d433196236ed61
PavelDonchenko <przmld033@gmail.com>
2022-07-02 13:12:48 +0300

    add search

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
-import Header from "./components/Header";
-import './scss/app.scss';
-import './assets/dynamic_adapt.js';
-
-import Home from "./Pages/Home";
-import Cart from "./Pages/Cart/Cart";
-import Footer from "./components/Footer";
-
+import React from 'react';
 import {
   Routes,
   Route,
 } from "react-router-dom";
 
+import Header from "./components/Header";
+import './scss/app.scss';
+import './assets/dynamic_adapt.js';
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart/Cart";
+import Footer from "./components/Footer";
+
+export const SearchContext = React.createContext()
+
+
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('')
   return (
     <div className="App">
       <div className="wrapper">
-        <Header />
-        <main className="page">
-          <Routes>
-            <Route path = '/' element =  {<Home />} />
-            <Route path = '/cart' element =  {<Cart />} />
-          </Routes>
-        </main>
-        <Footer />
+        <SearchContext.Provider value = {{searchValue, setSearchValue}}>
+          <Header />
+          <main className="page">
+            <Routes >
+              <Route path = '/' element =  {<Home />} />
+              <Route path = '/cart' element =  {<Cart />} />
+            </Routes>
+          </main>
+          <Footer />
+        </SearchContext.Provider>
       </div>
     </div>
   );
