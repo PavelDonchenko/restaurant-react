9117e512644364efc2ae1ca70e370e6df7f6b919
PavelDonchenko <przmld033@gmail.com>
2022-07-03 22:03:48 +0300

    add useCallback on search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { SearchContext } from '../../App'
+import debounce from 'lodash.debounce';
 
 function Search() {
-    const {searchValue, setSearchValue} = React.useContext(SearchContext);
+    const [value, setValue] = React.useState('');
+    const {setSearchValue} = React.useContext(SearchContext);
+
+
+    const updateSearchValue = React.useCallback(debounce((str) => {
+        setSearchValue(str)
+    },1000),
+    []
+    )
+    const onChangeInput = (event) => {
+        setValue(event.target.value)
+        updateSearchValue(event.target.value)
+    }
     return (
         <form className="top-header__form form-header">
             <div className="form-header__container">
                 <input 
-                onChange = {event => setSearchValue(event.target.value)}
-                value = {searchValue}
+                onChange = {onChangeInput}
+                value = {value}
                 className="form-header__input" 
                 placeholder="Поиск блюда" 
                 type="search" />
